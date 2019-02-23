4c34b267f9241a761886aebc66cd498ec49023ff
hsp <907172835@qq.com>
2019-02-23 14:18:46 +0800

    V4.0

--- src/main/java/com/baizhi/service/impl/UserServiceImpl.java
+++ src/main/java/com/baizhi/service/impl/UserServiceImpl.java
@@ -1,15 +1,15 @@
 package com.baizhi.service.impl;
 
 import com.baizhi.dao.UserMapper;
+import com.baizhi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.Serializable;
 
 @Service
 @Transactional
-public class UserServiceImpl implements Serializable {
+public class UserServiceImpl implements UserService {
     @Autowired
     private UserMapper userMapper;
 }
